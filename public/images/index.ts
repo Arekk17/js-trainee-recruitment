183263111fced63bb7fe27d0533a17600fcb13ec
Arekk77 <daszkowskiarek145@gmail.com>
2023-03-14 13:21:25 +0100

    adding photos creating exports

---
+++ public/images/index.ts
@@ -0,0 +1,9 @@
+export const images = {
+    polarbear: "/images/polarbear.png",
+    cheetath: "/images/cheetah.png",
+    elephant: "/images/elephant.png",
+    fox: "/images/fox.png",
+    panda: "/images/panda.png",
+    squirrel: "/images/squirrel.png",
+    butterfly: "/images/butterfly.png"
+}
